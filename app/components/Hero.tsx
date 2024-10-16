af2431d6112c0b62e95a96d79b549dd79ca341f2
AyoubProjects28 <ayoubalapro28@gmail.com>
2024-10-17 00:35:49 +0200

    Added Spring and Angular Icons in intro

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,12 @@
 import { ComponentPropsWithoutRef } from "react"
 import { Section } from "./Section"
 import { cn } from "@/lib/utils";
+import { AngularIcon } from "./icons/AngularIcon";
+import { SpringIcon } from "./icons/SpringIcon";
 
 
 const Code = ({ className, ...props}: ComponentPropsWithoutRef<"span">) => {
-    return <span {...props} className={cn("bg-accent/30 font-mono border-accent p-1 -m-1", className)}/>;
+    return <span {...props} className={cn("bg-accent/30 font-mono border border-accent p-1 rounded-sm text-primary whitespace-nowrap", className)}/>;
 }
 
 export const Hero = () => {
@@ -12,7 +14,14 @@ export const Hero = () => {
         <div className="flex-[2]">
             <h2 className="font-caption text-5xl text-primary">Ayoub Ala</h2>
             <h3 className="text-3xl font-caption">Software Engineer Junior</h3>
-            <p>With a strong focus on back-end development, I specialize in <Code>Angular</Code> / <Code>Spring</Code>, 
+            <p>With a strong focus on back-end development, I'm specialized in {" "}
+                <Code>
+                    <AngularIcon size={16} className="inline"/> Angular
+                </Code>
+                 - 
+                <Code>
+                    <SpringIcon size={16} className="inline"/> Spring
+                </Code>, 
                 but I enjoy leveraging my broader skillset to tackle diverse challenges across the stack.
             </p>
         </div>
