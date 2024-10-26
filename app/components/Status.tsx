f8a6afb1efa4745cfc5eb36f4e90e8fcc9e4d3bd
AyoubProjects28 <ayoubalapro28@gmail.com>
2024-10-26 15:48:14 +0200

    Increased size of section, created Work section

--- app/components/Status.tsx
+++ app/components/Status.tsx
@@ -1,7 +1,8 @@
 import { Card } from "@/components/ui/card"
 import { Section } from "./Section"
-import { BriefcaseBusiness, Code, Link, LucideIcon, School, ShoppingCart } from "lucide-react"
+import { BriefcaseBusiness, LucideIcon, School, ShoppingCart } from "lucide-react"
 import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
 
 export const Status = () => {
     return (
@@ -23,6 +24,11 @@ export const Status = () => {
                     <p className="text-lg text-muted-foreground">
                         Work
                     </p>
+                    <div className="flex flex-col gap-4">
+                        {WORKS.map((work,index) => (
+                            <Work key={index} {...work}/>
+                        ))}
+                    </div>
                 </Card>
                 <Card className="p-4 flex-1">
                     <p className="text-lg text-muted-foreground">
@@ -34,7 +40,7 @@ export const Status = () => {
     )
 };
 
-const SIDE_PROJECTS = [
+const SIDE_PROJECTS : SideProjectProps[] = [
     {
         logo: School,
         title: "Private Tutoring Platform",
@@ -96,6 +102,67 @@ const SideProject = (props: SideProjectProps) => {
                 </p>
 
             </div>
+        </a>
+    )
+};
+
+const WORKS: WorkProps[] = [
+    {
+        image: "https://media.licdn.com/dms/image/v2/D4D0BAQH-ZV832H4sdA/company-logo_100_100/company-logo_100_100/0/1705572256355/capgemini_logo?e=1737590400&v=beta&t=cFeZ_d7AMDds_Ad3cPnsrNBWXBJtXhGFdd9a0sFp7eE",
+        company: "Capgemini",
+        role: "Software Engineer Angular/Java for Stellantis",
+        date: "Fév.–Juill. 2024",
+        url: "/",
+        stage: true
+    },
+    {
+        image: "https://media.licdn.com/dms/image/v2/D4D0BAQEAY4v1FlZPuw/company-logo_100_100/company-logo_100_100/0/1725886352360/anyosfr_logo?e=1737590400&v=beta&t=XZFoKzlPRUiL3w9oaLVBBsJSFEV57CINVCq0qdDyNmg",
+        company: "ANYOS",
+        role: "Embedded Software Engineer",
+        date: "Janv.–Juin. 2022",
+        url: "/",
+        stage: true
+    },
+    {
+        image: "https://media.licdn.com/dms/image/v2/D4E0BAQGrA-W-ZIsOoQ/company-logo_100_100/company-logo_100_100/0/1682510702496/xefi_logo?e=1737590400&v=beta&t=68FMYGWAyJz9CHJkt3kUVXOgw7pi_V25VLH2RepXgSk",
+        company: "XEFI",
+        role: "IT Technician",
+        date: "Juin–Juill. 2023",
+        url: "/",
+        stage: true
+    }
+];
+
+type WorkProps = {
+    image: string,
+    company: string,
+    role: string,
+    date: string,
+    url?: string,
+    stage?: boolean
+};
+
+const Work = (props: WorkProps) => {
+    return (
+        <a href={props.url} className="inline-flex items-center justify-between w-full hover:bg-accent/50 transition-colors p-1 rounded">
+            <div className="gap-4 inline-flex items-center">
+                <img src={props.image} alt={props.company} className="w-10 h-10 object-contain rounded-md"/>
+
+                <div className="mr-auto">
+                    <div className="flex items-center gap-2">
+                        <p className="text-lg font-semibold">
+                            {props.company}
+                        </p>
+                        {props.stage && <Badge variant="outline">Stage</Badge>}
+                    </div>
+                    <p className="text-xs text-muted-foreground">
+                        {props.role}
+                    </p>
+                </div>
+            </div>
+            <p className="text-xs text-end text-muted-foreground">
+                {props.date}
+            </p>
         </a>
     )
 };
