ecd393c592d53086096ee3b9b3f339a9441abf63
AyoubProjects28 <ayoubalapro28@gmail.com>
2024-10-28 01:54:10 +0100

    Download CV Button done, header visible on sticky mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,9 +8,20 @@ import { InstagramIcon } from "./icons/InstagramIcon"
 
 export const Header = () => {
     return (
-        <header className="sticky top-0 py-4">
-            <Section className="flex items-baseline">
+        <header className="sticky top-0 py-3 bg-background z-50">
+            <Section className="flex items-baseline justify-center">
                 <h1 className="text-lg font-bold text-primary">ayoubala.com</h1>
+                <div className="flex-1"/>
+                <a
+                    href="/CV_Ayoub_ALA_SofE.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    download
+                    className="hidden sm:block font-bold text-background bg-primary py-2 px-4 rounded-full transition duration-300 ease-in-out hover:scale-105 hover:bg-primary/90"
+                    aria-label="Download CV"
+                >
+                    Download CV
+                </a>
                 <div className="flex-1"/>
                 <ul className="flex items-center gap-2">
                     <Link href="https://github.com/AyoubProjects28" className={cn(buttonVariants({ variant: "outline" }))}>
